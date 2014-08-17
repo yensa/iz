cfcdd52eacc2ccd1d02e18b917c630b346a616e6
Basile Burg <basile.burg@gmx.com>
2014-08-17 23:45:33 +0200

    updated scripts

--- scripts/ddocs.bat
+++ scripts/ddocs.bat
@@ -1,2 +1,2 @@
-dmd -D -o- -X -Xf"..\doc\docs.json" -I"..\import" "..\import\iz\types.d" "..\import\iz\bitsets.d" "..\import\iz\streams.d" "..\import\iz\containers.d" "..\import\iz\properties.d" "..\import\iz\referencable.d" "..\import\iz\serializer.d" -Dd"..\doc"
+dmd -D -o- -X -Xf"..\doc\docs.json" -I"..\import" "..\import\iz\types.d" "..\import\iz\bitsets.d" "..\import\iz\observer.d" "..\import\iz\streams.d" "..\import\iz\containers.d" "..\import\iz\properties.d" "..\import\iz\referencable.d" "..\import\iz\serializer.d" -Dd"..\doc"
 pause
